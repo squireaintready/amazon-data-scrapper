eb61dc3afa4d56e33c5d5cd0f44003f4547940e5
Samuel Jo <jo.sunghak@gmail.com>
2021-07-19 12:22:43 -0400

    make it better

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const request = require('request-promise')
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const generateScraperUrl = (apiKey) => `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`
+
+app.use(express.json());
+
+app.get('/', (req, res) =>{
+  res.send('Welcome to Amazon Scrapper API.')
+});
+
+// GET PRODUCT DETAILS
+app.get('/products/:productId?api_key=', async(req, res) =>{
+  const { productId } = req.params;
+  const { api_key } = req.query;
+
+  try{
+    const response = await request(`${generateScraperUrl(apiKey)}&url=https://www.amazon.com/dp/${productId}`)
+    res.json(JSON.parse(response));
+  }catch(error){
+    res.json(error)
+  }
+})
+
+// GET PRODUCT REVIEWS
+app.get('/products/:productId/reviews', async(req, res) =>{
+  const { productId } = req.params;
+  const { api_key } = req.query;
+
+  try{
+    const response = await request(`${generateScraperUrl(apiKey)}&url=https://www.amazon.com/product-reviews/${productId}`)
+    res.json(JSON.parse(response));
+  }catch(error){
+    res.json(error)
+  }
+})
+
+// GET PRODUCT OFFERS
+app.get('/products/:productId/offers', async(req, res) =>{
+  const { productId } = req.params;
+  const { api_key } = req.query;
+
+  try{
+    const response = await request(`${generateScraperUrl(apiKey)}&url=https://www.amazon.com/gp/offer-listing/${productId}`)
+    res.json(JSON.parse(response));
+  }catch(error){
+    res.json(error)
+  }
+})
+
+// GET SEARCH RESULTS
+app.get('/search/:searchQuery', async(req, res) =>{
+  const { searchQuery } = req.params;
+  const { api_key } = req.query;
+  try{
+    const response = await request(`${generateScraperUrl(apiKey)}&url=https://www.amazon.com/s?k=${searchQuery}`)
+    res.json(JSON.parse(response));
+  }catch(error){
+    res.json(error)
+  }
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
